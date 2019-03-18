284b278b46645be265c08815c69c8631c8ea6df7
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-03-18 09:05:17 +0700

    feat(thrift): create several component & register thrift service in main app

---
+++ component/header.component.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Link href="/">
+                    <a>Home Page</a>
+                </Link>
+                <Link href="/about">
+                    <a>About Page</a>
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../component/header.component';
+
+const index = () => (
+    <div>
+        <Header />
+        <p>Hello Next.JS</p>
+    </div>
+);
+
+export default index;
